a4d0fc5a4b0727894c7798b408c11cbf910d6a29
Bukiatorrr <bukinator0272@gmail.com>
2019-12-04 02:34:26 +0400

    Temp DataHolder

--- src/main/java/service/DataHolder.java
+++ src/main/java/service/DataHolder.java
@@ -8,30 +8,84 @@ import java.util.List;
 
 public class DataHolder {
 
-    private List<Track> traks;
+    private List<Track> tracks;
     private List<Genre> genres;
 
     public DataHolder() {
-        traks = new ArrayList<>();
+        tracks = new ArrayList<>();
         genres = new ArrayList<>();
     }
 
-    public List<Track> getAlbums() {
-        return traks;
-    }
-
-    public List<Genre> getGenres() {
-        return genres;
-    }
-
     void addTrack(Track track) {
-        traks.add(track);
+        tracks.add(track);
 //        GenreDAO genreDAO = new GenreDAO();
 //        genreDAO.add(track);
     }
 
     void addGenre(Genre genre) {
         genres.add(genre);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(genre);
+    }
+
+    public List<Track> getAlbums() {
+        return tracks;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public int getTraksCount() {
+        return tracks.size();
+    }
+
+    public int getGenresCount() {
+        return genres.size();
+    }
+
+    //wrong argument
+    public Track getTrack(Track track) {
+        tracks.add(track);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(track);
+        return track;
+    }
+
+    //wrong argument
+    public Genre getGenre(Genre genre) {
+        genres.add(genre);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(genre);
+        return genre;
+    }
+
+    //wrong argument
+    //wrong statement
+    void updateTrack(Track track) {
+        tracks.add(track);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(track);
+    }
+
+    //wrong argument
+    //wrong statement
+    void updateGenre(Genre genre) {
+        genres.add(genre);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(genre);
+    }
+
+    //wrong argument
+    void deleteTrack(Track track) {
+        tracks.remove(track);
+//        GenreDAO genreDAO = new GenreDAO();
+//        genreDAO.add(track);
+    }
+
+    //wrong argument
+    void deleteGenre(Genre genre) {
+        genres.remove(genre);
 //        GenreDAO genreDAO = new GenreDAO();
 //        genreDAO.add(genre);
     }
